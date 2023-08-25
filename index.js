f41a40c06c63fede87d4cef15ed9656c50334dd6
matax123 <matias.ortiz@alumnos.ucn.cl>
2023-08-25 18:06:22 -0400

    change from vue to javascript

---
+++ index.js
@@ -0,0 +1,22 @@
+new WOW().init()
+
+function openModalWindow(){
+    console.log("openModalWindow");
+    document.getElementById("modalWindow").style.visibility = "visible";
+}
+
+function openModalWindowText(){
+    console.log("openModalWindowText");
+    document.getElementById("modalWindowText").style.visibility = "visible";
+}
+
+function createText(text, color, backgroundColor, size, font, ){
+    //create element
+    var text = document.createElement("p");
+
+}
+
+function saveWebpage(){
+    console.log("saveWebpage");
+    //se manda html a backend
+}
